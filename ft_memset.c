cb260f2c65898f0efdaf16faf5942c849e1cd0d5
jose-mim <jose-mim@student.42malaga.com>
2025-04-24 19:33:43 +0200

    memset function

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 19:23:04 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/04/24 19:33:43 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *ptr;
+	size_t i;
+
+	ptr = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
