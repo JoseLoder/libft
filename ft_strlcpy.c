cb31d3e8b08ed7f389ceff64fb29c0d09e4666d7
jose-mim <jose-mim@student.42malaga.com>
2025-04-26 18:24:22 +0200

    strlcpy function

---
+++ ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 17:14:16 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/04/26 18:23:51 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t src_len;
+	size_t i;
+
+	src_len = ft_strlen(src);
+
+	if (size == 0)
+	{
+		return (src_len);
+	}
+
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+
+	return (src_len);
+}
