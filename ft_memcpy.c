c621cdfc56d1b7ec835bedfbbf1345a040f9dbdf
jose-mim <jose-mim@student.42malaga.com>
2025-04-26 14:26:54 +0200

    memcpy function

---
+++ ft_memcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 13:52:23 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/04/26 14:25:52 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *ptr_d;
+	unsigned char *ptr_s;
+	size_t i;
+
+	if (!dest && !src)
+		return (NULL);
+
+	ptr_d = (unsigned char *)dest;
+	ptr_s = (unsigned char *)src;
+	i = 0;
+
+	while (i < n)
+	{
+		ptr_d[i] = ptr_s[i];
+		i++;
+	}
+	return (dest);
+}
