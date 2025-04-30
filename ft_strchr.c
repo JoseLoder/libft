375f11dbae50902eafd5ab13d11dcb5b388d94c0
jose-mim <jose-mim@student.42malaga.com>
2025-04-30 18:21:52 +0200

    Remove commentary

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:02:30 by jose-mim          #+#    #+#             */
-/*   Updated: 2025/04/30 17:22:47 by jose-mim         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:21:06 by jose-mim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	while (*s != '\0')
 	{
 		if (*s == character)
+		{
 			return ((char *)s);
+		}
 		s++;
 	}
-	// Verificar si c es '\0' y devolver un puntero al final de la cadena
 	if (character == '\0')
 		return ((char *)s);
 
