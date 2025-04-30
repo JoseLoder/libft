7a18d7230154cd3a6e4e103c6652595ec297cdb2
jose-mim <jose-mim@student.42malaga.com>
2025-04-30 19:24:59 +0200

    strncmp function

---
+++ ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 18:36:01 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/04/30 19:24:42 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (i < n && (str1[i] != '\0' && str2[i] != '\0'))
+	{
+		if (str1[i] != str2[i])
+		{
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
