f6571b3673e389ed11985b6816e679c384d7bbb8
jose-mim <jose-mim@student.42malaga.com>
2025-04-30 16:51:25 +0200

    Remove commentary

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 18:32:20 by jose-mim          #+#    #+#             */
-/*   Updated: 2025/04/30 16:38:15 by jose-mim         ###   ########.fr       */
+/*   Updated: 2025/04/30 16:38:36 by jose-mim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 
-	// Si el tamaño del buffer es menor o igual a la longitud de dst,
-		no se puede concatenar
 	if (size <= dst_len)
 		return (size + src_len);
 
 	i = dst_len;
 	j = 0;
 
-	// Copiar src a dst mientras haya espacio disponible
 	while (i < size - 1 && src[j] != '\0')
 	{
 		dst[i] = src[j];
@@ -38,9 +35,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		j++;
 	}
 
-	// Asegurar que dst esté terminado en '\0'
 	dst[i] = '\0';
 
-	// Retornar la longitud total que se intentó crear
 	return (dst_len + src_len);
 }
