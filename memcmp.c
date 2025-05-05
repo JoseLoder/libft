148cbc8bbb73beec672ccb544f9528add1d3aeb5
jose-mim <jose-mim@student.42malaga.com>
2025-05-05 18:17:04 +0200

    memcmp function

---
+++ memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcmp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 17:51:01 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/05/05 18:16:25 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	const unsigned char *ptr1;
+	const unsigned char *ptr2;
+
+	i = 0;
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+
+	while (i < n)
+	{
+		if (ptr1[i] != ptr2[i])
+		{
+			return (ptr1[i] - ptr2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
