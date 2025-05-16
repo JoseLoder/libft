89cca06b0192ab4725b8dc91158269d18ca857eb
jose-mim <jose-mim@student.42malaga.com>
2025-05-16 22:10:43 +0200

    folder from-mac changes

--- from-mac/ft_atoi.c
+++ from-mac/ft_atoi.c
@@ -1,93 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-mim <jose-mim@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 20:45:06 by jose-mim          #+#    #+#             */
+/*   Updated: 2025/05/16 20:47:09 by jose-mim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 
-int ft_isdigit(int digit)
+int	ft_atoi(const char *str)
 {
-    if (digit >= '0' && digit <= '9')
-        return (1);
-    else
-        return (0);
-}
+	int	result;
+	int	sign;
+	int	i;
+	int	j;
 
-int ft_atoi(const char *str)
-{
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    int j = 0;
-
-    // Handle leading whitespace
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-           str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
-    {
-        i++;
-    }
-
-    while (str[i])
-    {
-        if (ft_isdigit(str[i]) || (str[i] == '-' && j == 0))
-        {
-            if (str[i] == '-')
-            {
-                sign = -1;
-            }
-            else
-            {
-                if (result > (2147483647 / 10) || (result == (2147483647 / 10) && (str[i] - '0') > 7))
-                {
-                    return (sign == 1) ? 2147483647 : -2147483648;
-                }
-                result = result * 10 + (str[i] - '0');
-            }
-            j++;
-        }
-        else if (j > 0)
-        {
-            return result * sign;
-        }
-        i++;
-    }
-    return result * sign;
-}
-
-int main(void)
-{
-    char buffer[10];
-    size_t result;
-
-    // Example 1
-    const char *str1 = "abc123def456";
-
-    if (result)
-        printf("Example 1: First sequence of digits starts at: %d\n", ft_atoi(str1));
-    else
-        printf("Example 1: No digits found in the string.\n");
-
-    // Example 2
-    const char *str2 = "xyz789uvw";
-
-    if (result)
-        printf("Example 2: First sequence of digits starts at: %d\n", ft_atoi(str2));
-    else
-        printf("Example 2: No digits found in the string.\n");
-
-    // Example 3
-    const char *str3 = "no_digits_here";
-
-    if (result)
-        printf("Example 3: First sequence of digits starts at: %d\n", ft_atoi(str3));
-    else
-        printf("Example 3: No digits found in the string.\n");
-
-    // Example 4
-    const char *str4 = "xyz-789";
-
-    if (result)
-    {
-        printf("Example 2: First sequence of digits starts at: %d\n", ft_atoi(str4));
-    }
-    else
-        printf("Example 2: No digits found in the string.\n");
-
-    return (0);
+	result = 0;
+	sign = 1;
+	i = 0;
+	j = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r'
+		|| str[i] == '\v' || str[i] == '\f')
+	{
+		i++;
+	}
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9' || (str[i] == '-' && j == 0))
+		{
+			if (str[i] == '-')
+			{
+				sign = -1;
+			}
+			else
+			{
+				if (result > (2147483647 / 10) || (result == (2147483647 / 10)
+						&& (str[i] - '0') > 7))
+				{
+					return ((sign == 1) ? 2147483647 : -2147483648);
+				}
+				result = result * 10 + (str[i] - '0');
+			}
+			j++;
+		}
+		else if (j > 0)
+		{
+			return (result * sign);
+		}
+		i++;
+	}
+	return (result * sign);
 }
